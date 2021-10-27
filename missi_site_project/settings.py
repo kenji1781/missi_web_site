2e311b71424460efab651b6f848a03354202192d
KenjiMishima <62679752+kenji1781@users.noreply.github.com>
2021-10-27 23:23:38 +0900

    css

--- missi_site_project/settings.py
+++ missi_site_project/settings.py
@@ -130,5 +130,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
