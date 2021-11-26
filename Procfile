3040595abd8e3357645c98341234cca741831be6
KenjiMishima <kenji1781@gmail.com>
2021-11-27 08:23:21 +0900

    Procfile

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: gunicorn missi_site_project.wsgi --log-file -
-release: bin/python db:migrate
--- missi_site_project/settings.py
+++ missi_site_project/settings.py
@@ -30,8 +30,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-#ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
-ALLOWED_HOSTS = ['*']
+
 
 # Application definition
 
@@ -194,7 +193,12 @@ try:
 except ImportError:
     pass
 
+if DEBUG:
+    ALLOWED_HOSTS = ['*']
+
+#heroku側のpythonシークレットキーを使う
 if not DEBUG:
+    AOWELLD_HOSTS = ['127.0.0.1', '.herokuapp.com']
     SECRET_KEY = os.environ['SECRET_KEY']
     import django_heroku
     django_heroku.settings(locals())
