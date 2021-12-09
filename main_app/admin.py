0250b2fe404999df0a890d66ffbde91bdf60f35d
KenjiMishima <62679752+kenji1781@users.noreply.github.com>
2021-12-09 13:04:33 +0900

    migrationやり直し

--- main_app/admin.py
+++ main_app/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from .models import Customer_Infomation,Equipment_Category,Machine_Model,Trouble_Contents,\
     Unit_Price_Electric,Unit_Price_Steam,Unit_Price_Gas,Unit_Price_Water,Solvent_Name,Solvent_Manufacturer,Solvent_Conf,\
         Customer_Machine,Trouble_History,Customer_Machine_Recipe,Machine_Drive_History,\
-            Cost_Electric,Cost_Steam,Cost_Gas,Cost_Water,Cost_Solvent,Cost_Total
+            Cost_Electric,Cost_Steam,Cost_Gas,Cost_Water,Cost_Solvent,Cost_Total,Mail_Notification,\
+                Trouble_Mail_Setting,Maintenance_Mail_Setting                
+
 
 admin.site.register(Customer_Infomation)
 admin.site.register(Equipment_Category)
@@ -25,5 +27,9 @@ admin.site.register(Cost_Gas)
 admin.site.register(Cost_Water)
 admin.site.register(Cost_Solvent)
 admin.site.register(Cost_Total)
+admin.site.register(Mail_Notification)
+admin.site.register(Trouble_Mail_Setting)
+admin.site.register(Maintenance_Mail_Setting)
+
 
 # Register your models here.
