a0cf4e8d5e9c2b026ae8810a1b471385d8b5e143
KenjiMishima <62679752+kenji1781@users.noreply.github.com>
2022-02-15 15:21:43 +0900

    fix

--- main_app/views/work_history_graph.py
+++ main_app/views/work_history_graph.py
@@ -59,9 +59,9 @@ class WorkHistoryGraphView(LoginRequiredMixin,TemplateView):
         #レシピモデルの補完を行う##########################
         recipemodelcomp = RecipeModelComplement()
         #idから機種を書込み
-        recipemodelcomp.machine_model_complement(object_list_recipe)
+        #recipemodelcomp.machine_model_complement(object_list_recipe)
         #品種No.から品種名を書込み
-        recipemodelcomp.recipe_model_complement(object_list_recipe)
+        #recipemodelcomp.recipe_model_complement(object_list_recipe)
 
 
 
@@ -70,9 +70,9 @@ class WorkHistoryGraphView(LoginRequiredMixin,TemplateView):
         #datetimeをdateとtimeに分割
         modelcomp.datetime_complement(queryset)
         #idから機種を書込み
-        modelcomp.machine_model_complement(queryset)
+        #modelcomp.machine_model_complement(queryset)
         #品種No.から品種名を書込み
-        modelcomp.recipe_model_complement(queryset)
+        #modelcomp.recipe_model_complement(queryset)
         #各最新単価を書込み
         modelcomp.unit_cost_complement(queryset)
         #################################################
